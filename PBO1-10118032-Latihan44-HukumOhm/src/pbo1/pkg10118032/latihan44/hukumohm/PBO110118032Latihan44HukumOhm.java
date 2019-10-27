66dfa4291511e195ddbaccb2105aadab52bc1726
Guntur Maulana Ibrahim <55784684+gunturmaulanaii@users.noreply.github.com>
2019-10-27 19:07:57 +0700

    Add files via upload

---
+++ PBO1-10118032-Latihan44-HukumOhm/src/pbo1/pkg10118032/latihan44/hukumohm/PBO110118032Latihan44HukumOhm.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pbo1.pkg10118032.latihan44.hukumohm;
+
+/**
+ *
+ * @author Guntur Maulana I
+ * NAMA  : Guntur Maulana Ibrahim
+ * KELAS : IF 1
+ * NIM   : 10118032
+ * Deskripsi Program : Menampilkan output Hukum Ohm.
+ */
+public class PBO110118032Latihan44HukumOhm {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Baterai baterai = new Baterai();
+        Baterai ohm = new Baterai(3,12);
+        System.out.println("Kuat Arus : " + ohm.getKuatArus() + " ampere");
+        System.out.println("Hambatan : " + ohm.getHambatan() + " ohm");
+        System.out.println("Hasil Tegangan : " + ohm.hitungTegangan()+ " volt");
+    }
+    
+}
